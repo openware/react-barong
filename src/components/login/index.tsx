900048ee0993cc22c920b64f127febbcb2ef251c
Kohelbekker <kohelbekker@gmail.com>
2020-02-21 11:47:24 +0200

    Feature: fields validation for login form

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -5,19 +5,44 @@ import axios from 'axios';
 interface LoginState {
   email: string,
   password: string,
+  emailValid: boolean,
+  passwordValid: boolean,
+  formValid: boolean,
 }
+
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; // min. 8 characters, at least one uppercase letter, lowercase letter, number and special character
+const EMAIL_REGEX = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
 
 export class LoginForm extends Component<LoginState> {
   public state = {
     email: '',
     password: '',
+    emailValid: false,
+    passwordValid: false,
+    formValid: false,
   };
 
   onChange = (value: string, key: string) => {
     // @ts-ignore
+    switch(key) {
+      case 'email':
+        value.match(EMAIL_REGEX) ? this.setState({emailValid: true}) : this.setState({emailValid: false});
+        break;
+      case 'password':
+        value.match(PASSWORD_REGEX) ? this.setState({passwordValid: true}) : this.setState({passwordValid: false});
+        break;
+      default:
+        break;
+    }
     this.setState({
         [key]: value,
-    });
+    }, this.validateForm);
+  }
+
+  validateForm = () => {
+    this.setState({
+      formValid: this.state.emailValid && this.state.passwordValid,
+    })
   }
 
   handleSubmit = (e: { preventDefault: () => void; }) => {
@@ -26,12 +51,18 @@ export class LoginForm extends Component<LoginState> {
         email: this.state.email,
         password: this.state.password,
     };
-    console.log(data.email, data.password);
-    const response = axios.post('http://localhost:3000/api/v2/identity/sessions',
-        { email: data.email,
-          password: data.password}
-        )
-    console.log(response);
+    axios.post('http://localhost:3000/api/v2/identity/sessions',
+      {
+        email: data.email,
+        password: data.password
+      }
+    )
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error.response)
+    });
   }
 
   render() {
@@ -49,7 +80,7 @@ export class LoginForm extends Component<LoginState> {
                 <span className="input-group-addon"><span className="glyphicon glyphicon-lock"></span></span>
                 <input type="password" className="form-control" name="password" placeholder="Password" onChange={e => this.onChange(e.target.value, 'password')} />
               </div>
-              <button className="btn btn-primary btn-block login-button" type="submit"><i className="fa fa-sign-in"></i> Login</button>
+              <button className="btn btn-primary btn-block login-button" type="submit" disabled={!this.state.formValid}><i className="fa fa-sign-in"></i> Login</button>
               <div className="login-options">
                 <a href="#" className="login-forgot">Forgot Password?</a>
               </div>
