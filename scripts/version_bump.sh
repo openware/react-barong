57d6bee1596256ce238122b91e24adb635661b33
Kohelbekker <kohelbekker@gmail.com>
2020-03-23 16:15:55 +0200

    Technical commit

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-DIFF="$(git diff --raw origin/master VERSION)"
-
-if [ -z "$DIFF" ]
-then
-    VERSION="patch"
-else
-    VERSION="$(cat VERSION)"
-fi
-
 npm --no-git-tag-version version "$VERSION"
 npm publish
-grep -Po '"version":.*?[^\\]",' package.json | cut -d "\"" -f 4 > VERSION
-git tag "v$(cat VERSION | cut -d "\"" -f 2)"
