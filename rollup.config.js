ed545aa7e97901af24f46d625b92a2df882aa466
Berezh <verezh@gmail.com>
2020-12-14 10:16:46 +0200

    fix build (#36)
    * fix build
    
    * update version +

--- rollup.config.js
+++ rollup.config.js
@@ -4,41 +4,50 @@ import pkg from './package.json';
 import external from 'rollup-plugin-peer-deps-external';
 import fs from 'fs';
 
+const scssOutput =
+    process.env.BUILD === 'dev'
+        ? function (styles, styleNodes) {
+              fs.writeFileSync('dist/index.scss', styles);
+              fs.writeFileSync('example/src/react-barong/index.scss', styles);
+          }
+        : 'dist/index.scss';
+
 const plugins = [
     external(),
     typescript({
         typescript: require('typescript'),
     }),
     scss({
-        output: true,
-        output: function (styles, styleNodes) {
-            fs.writeFileSync('dist/index.scss', styles);
-            fs.writeFileSync('example/src/react-barong/index.scss', styles);
-        },
+        output: scssOutput,
     }),
 ];
 
+const output = [
+    {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+    },
+    {
+        file: pkg.module,
+        format: 'es',
+        exports: 'named',
+        sourcemap: true,
+    },
+];
+
+if (process.env.BUILD === 'dev') {
+    output.push({
+        file: 'example/src/react-barong/index.js',
+        format: 'es',
+        banner: '/* tslint-disable */',
+    });
+}
+
 export default {
     input: 'src/index.tsx',
-    output: [
-        {
-            file: pkg.main,
-            format: 'cjs',
-            exports: 'named',
-            sourcemap: true,
-        },
-        {
-            file: pkg.module,
-            format: 'es',
-            exports: 'named',
-            sourcemap: true,
-        },
-        {
-            file: 'example/src/react-barong/index.js',
-            format: 'es',
-            banner: '/* tslint-disable */',
-        },
-    ],
+    output,
     plugins,
     external: ['react-hook-form', 'react-bootstrap', 'axios'],
 };
